6b04568d77d78896a5ca5e72b273f350c5f34b9a
Sander Vocke <sandervocke@gmail.com>
2023-08-31 08:07:55 +0000

    WIP

--- test/run_example.py
+++ test/run_example.py
@@ -10,6 +10,7 @@ example = sys.argv[1]
 
 QOVERAGE = os.environ.get("QOVERAGE", f"python {script_dir}/../qoverage.py")
 QML = os.environ.get("QML", "qml")
+DUMP_RUN_LOG = os.environ.get("DUMP_RUN_LOG", "0") != "0"
 output_dir = tempfile.mkdtemp()
 
 print(f"Using qoverage command: {QOVERAGE}")
@@ -26,6 +27,12 @@ subprocess.run(command, shell=True, check=True)
 command = f"timeout 3s {QML} {output_dir}/main.qml 2>&1 | tee {output_dir}/run.log"
 print(f"Running:\n  -> {command}")
 subprocess.run(command, shell=True, check=True)
+if DUMP_RUN_LOG:
+    try:
+        with open(f"{output_dir}/run.log", 'r') as f:
+            print(f.read())
+    except Exception:
+        print("No run log was found.")
 
 # Report
 command = f"{QOVERAGE} collect --report {output_dir}/report.xml --files-path {output_dir} --input {output_dir}/run.log"
